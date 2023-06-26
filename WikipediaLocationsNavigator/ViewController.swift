67acdfecdde6876f1be15f97a9b8531d466838d9
Jayesh Kawli <j.kawli@gmail.com>
2023-06-26 13:16:25 +0200

    Initial Commit

---
+++ WikipediaLocationsNavigator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikipediaLocationsNavigator
+//
+//  Created by Jayesh Kawli on 6/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
